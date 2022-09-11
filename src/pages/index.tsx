252476f7d6b1138bbbae5311047048b9e2705801
Agacy junior <juniormartinssom@gmail.com>
2022-09-11 10:48:56 -0300

    Data Fetching no Nextjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,16 +10,18 @@ import camiseta4 from "../assets/4.png";
 
 import "keen-slider/keen-slider.min.css";
 
-export default function Home() {
+export default function Home(props) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
-      spacing: 48
-    }
+      spacing: 48,
+    },
   });
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
+
       <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
@@ -57,4 +59,16 @@ export default function Home() {
       </Product>
     </HomeContainer>
   );
+}
+
+export const getServerSideProps = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
+  console.log('rodou')
+
+  return {
+    props: {
+      list: [1,2,3]
+    }
+  }
 }
