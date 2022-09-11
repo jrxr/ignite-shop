9e896aed6b65bd1482fd5d35648510123f5e7ed1
Agacy junior <juniormartinssom@gmail.com>
2022-09-11 18:15:35 -0300

    Botão de comprar

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,7 @@
-import { GetStaticPaths, GetStaticProps } from "next"
+import axios from "axios";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/future/image";
+import { useState } from "react";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
 import {
@@ -15,13 +17,30 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
-    defaultPriceId: string
+    defaultPriceId: string;
   };
 }
 
 export default function Product({ product }: ProductProps) {
-  function handleBuyButton() {
-    console.log(product.defaultPriceId);
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false);
+
+  async function handleBuyButton() {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post("/api/checkout", {
+        priceId: product.defaultPriceId,
+      });
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+    } catch (err) {
+      setIsCreatingCheckoutSession(false);
+
+      alert("Falha ao redirecionar ao checkout!");
+    }
   }
 
   return (
@@ -36,9 +55,9 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-      <button onClick={handleBuyButton}>
-        Comprar agora
-      </button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyButton}>
+          Comprar agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   );
@@ -46,18 +65,18 @@ export default function Product({ product }: ProductProps) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [
-      { params: { id: 'prod_MPfTkUcJ2lw0EW' } },
-    ],
-    fallback: 'blocking'
-  }
-}
+    paths: [{ params: { id: "prod_MPfTkUcJ2lw0EW" } }],
+    fallback: "blocking",
+  };
+};
 
-export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
   const productId = params.id;
 
   const product = await stripe.products.retrieve(productId, {
-    expand: ['default_price']
+    expand: ["default_price"],
   });
 
   const price = product.default_price as Stripe.Price;
@@ -68,14 +87,14 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
         id: product.id,
         name: product.name,
         imageUrl: product.images[0],
-        price: new Intl.NumberFormat('pt-BR', {
-          style: 'currency',
-          currency: 'BRL'
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
         }).format(price.unit_amount / 100),
         description: product.description,
-        defaultPriceId: price.id
-      }
+        defaultPriceId: price.id,
+      },
     },
-    revalidate: 60 * 60 * 1 // 1 hours
-  }
-}
+    revalidate: 60 * 60 * 1, // 1 hours
+  };
+};
